1e77fe9079e534a4e3f1237b827417d9d88ec950
Orbis Alonzo Gutierrez <orbisalonzo@gmail.com>
2022-05-04 22:01:14 -0400

    feat: the vowel-count was added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # algorithms
 List of fix algorithms
+
+- 1. 😁 [vowel-count](https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/csharp) : Count the number of vowels in a string.
---
+++ vowel-count/Vowel-count/Program.cs
@@ -0,0 +1,41 @@
+﻿
+/**
+    Return the number (count) of vowels in the given string.
+    We will consider a, e, i, o, u as vowels for this Kata (but not y).
+    The input string will only consist of lower case letters and/or spaces.
+
+ */
+
+
+Console.WriteLine("Hey type a text \n");
+
+void Main()
+{
+    string? text = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(text))
+    {
+        Console.WriteLine("Constains a 0 vowels");
+        return;
+    }
+
+    int count = 0;
+    char[] vowels = new char[5] { 'a', 'e', 'i', 'o', 'u' };
+    var list = text.ToLower().ToArray();
+
+
+    foreach (var item in list)
+    {
+        foreach (var vowel in vowels)
+        {
+            if (item.Equals(vowel))
+                count++;
+        }
+    }
+
+    Console.WriteLine($"Counstains a {count} vowels");
+}
+
+Main();
+
+Console.ReadKey();
